64131831eb6699b082520775827875b76f2e334d
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-10-01 11:12:14 +0200

    updates

--- hive.py
+++ hive.py
@@ -2,13 +2,15 @@ import random
 import math
 
 BEEHIVE_POS = (500, 500)
-MUTATE_RATE = 0.05
+MUTATE_RATE = 0.04
 POPULATION_SIZE = 100
-POPULATION_RATE = 0.7
+POPULATION_RATE = 0.2
+ELITE_COUNT = 5 
 
-# random.seed(42) # Uncomment this line to get the same results every time
+#random.seed(42)  # Uncomment this line to get the same results every time
 
-def init_flowers_list(): # Read the flowers from the file and return a list of positions
+
+def init_flowers_list():  # Read the flowers from the file and return a list of positions
     with open("field.txt", "r") as file:
         lines = file.readlines()
     pos = []
@@ -17,13 +19,14 @@ def init_flowers_list(): # Read the flowers from the file and return a list of p
         pos.append(((int(values[0])), (int(values[1]))))
     return pos
 
-class Bee: # The bee class represents a bee that has a path to follow and a distance to the flowers 
+
+class Bee:  # The bee class represents a bee that has a path to follow and a distance to the flowers
     def __init__(self):
         self.flowers = init_flowers_list()
         self.build_random_path()
         self.calculate_fitness()
 
-    def build_random_path(self): # Build a random path to the flowers 
+    def build_random_path(self):  # Build a random path to the flowers
         self.path = random.sample(self.flowers, len(self.flowers))
         return self.path
 
@@ -36,18 +39,23 @@ class Bee: # The bee class represents a bee that has a path to follow and a dist
             )
             actual_pos = (flower[0], flower[1])
         self.distance += math.sqrt(
-            ((actual_pos[0] - BEEHIVE_POS[0]) ** 2) + ((actual_pos[1] - BEEHIVE_POS[1]) ** 2)
+            ((actual_pos[0] - BEEHIVE_POS[0]) ** 2)
+            + ((actual_pos[1] - BEEHIVE_POS[1]) ** 2)
         )
         return self.distance
-    
+
     def mutate(self, paths):
-        nb_of_pos_mutated = int(len(paths) * MUTATE_RATE)
+        nb_of_pos_mutated = max(1, int(len(paths) * MUTATE_RATE))  # Assurer au moins une mutation
         pos_mutate = random.sample(range(len(paths)), nb_of_pos_mutated)
         for i in range(0, len(pos_mutate) - 1, 2):
             id, id2 = pos_mutate[i], pos_mutate[i + 1]
             paths[id], paths[id2] = paths[id2], paths[id]
         return paths
 
+    def crossover(self, other_bee):
+        cut = random.randint(0, len(self.path) - 1)
+        new_path = self.path[:cut] + [flower for flower in other_bee.path if flower not in self.path[:cut]]
+        return new_path
 
 
 class Hive:
@@ -73,17 +81,29 @@ class Hive:
         self.total_mutations += 1
         return new_bee
 
+    def create_crossover_bee(self, parent1, parent2):
+        new_bee = Bee()
+        new_bee.path = parent1.crossover(parent2)
+        new_bee.calculate_fitness()
+        return new_bee
+
     def generate_new_population(self, selected_bees):
         new_population = []
         while len(new_population) < POPULATION_SIZE:
-            for selected_bee in selected_bees:
-                if len(new_population) < POPULATION_SIZE:
-                    new_bee = self.create_mutated_bee(selected_bee)
-                    new_population.append(new_bee)
-                    self.total_bees_generated += 1
+            if random.random() < MUTATE_RATE:
+                parent = random.choice(selected_bees)
+                new_bee = self.create_mutated_bee(parent)
+            else:
+                parent1, parent2 = random.sample(selected_bees, 2)
+                new_bee = self.create_crossover_bee(parent1, parent2)
+            new_population.append(new_bee)
+            self.total_bees_generated += 1
         return new_population
+
+
 
     def select_and_breed(self):
         selected_bees = self.select_best_bees()
         self.population = self.generate_new_population(selected_bees)
         self.evaluate_population()
+
--- main.py
+++ main.py
@@ -1,68 +1,101 @@
 import matplotlib.pyplot as plt
-from hive import Hive, BEEHIVE_POS, POPULATION_SIZE, MUTATE_RATE
+from hive import Hive, BEEHIVE_POS, POPULATION_SIZE, MUTATE_RATE, POPULATION_RATE,  ELITE_COUNT
 
-def plot_field(flowers, BEEHIVE_POS): # Plot the field with the flowers and the hive 
+
+def plot_field(flowers, BEEHIVE_POS):  # Plot the field with the flowers and the hive
     x, y = zip(*flowers)
-    plt.scatter(x, y, c='blue', label='Flowers')
-    plt.scatter(*BEEHIVE_POS, c='red', label='Hive')
-    plt.title('Field')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.scatter(x, y, c="blue", label="Flowers")
+    plt.scatter(*BEEHIVE_POS, c="red", label="Hive")
+    plt.title("Field")
+    plt.xlabel("X")
+    plt.ylabel("Y")
     plt.legend()
     plt.show()
+
 
 def plot_best_distances(best_distances):
     plt.plot(best_distances)
-    plt.title('Best Distance per Generation')
-    plt.xlabel('Generation')
-    plt.ylabel('Best Distance')
-    plt.show()    
+    plt.title("Best Distance per Generation")
+    plt.xlabel("Generation")
+    plt.ylabel("Best Distance")
+    plt.show()
 
-def plot_best_path(best_bee, BEEHIVE_POS, generation, best_distance, total_mutations, total_bees_generated): # Plot the best path found by the bees 
+
+def plot_best_path(
+    best_bee,
+    BEEHIVE_POS,
+    generation,
+    best_distance,
+    total_mutations,
+    total_bees_generated,
+):  # Plot the best path found by the bees
     path = [BEEHIVE_POS] + best_bee.path + [BEEHIVE_POS]
     x, y = zip(*path)
-    plt.plot(x, y, marker='o')
-    plt.scatter(*BEEHIVE_POS, c='red', label='Hive', zorder=5)
-    plt.title('Best path found')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.plot(x, y, marker="o")
+    plt.scatter(*BEEHIVE_POS, c="red", label="Hive", zorder=5)
+    plt.title("Best path found")
+    plt.xlabel("X")
+    plt.ylabel("Y")
     plt.legend()
 
-   
-    textstr = ' '.join((
-        f'Generations: {generation}',
-        f'Best distance: {best_distance:.2f}',
-        f'Total mutations: {total_mutations}',
-        f'Total bees generated: {total_bees_generated}',
-        f'Population size: {POPULATION_SIZE}',
-        f'Mutate rate: {MUTATE_RATE}'
-    ))
-    plt.figtext(0.5, 0.95, textstr, fontsize=8, ha='center', bbox=dict(facecolor='white', alpha=0.5))
+    textstr = " ".join(
+        (
+            f"Generations: {generation}",
+            f"Best distance: {best_distance:.2f}",
+            f"Total mutations: {total_mutations}",
+            f"Total bees generated: {total_bees_generated}",
+            f"Population size: {POPULATION_SIZE}",
+            f"Mutate rate: {MUTATE_RATE}",
+            f"Population rate: {POPULATION_RATE}",
+            f"Elite count: {ELITE_COUNT}",
+        )
+    )
+    plt.figtext(
+        0.5,
+        0.95,
+        textstr,
+        fontsize=8,
+        ha="center",
+        bbox=dict(facecolor="white", alpha=0.5),
+    )
 
     plt.show()
 
-def main(num_generations): # Main function to run the genetic algorithm 
+
+def main(num_generations):  # Main function to run the genetic algorithm
     hive = Hive()
     generation = 0
     best_distance = hive.best_bee.distance
     best_distances = [best_distance]
 
-    while generation < num_generations: # Evolve the population for a specified number of generations
+    while (
+        generation < num_generations
+    ):  # Evolve the population for a specified number of generations
         hive.select_and_breed()
         generation += 1
         if hive.best_bee.distance < best_distance:
             best_distance = hive.best_bee.distance
-        best_distances.append(hive.best_bee.distance)  # Ajout de la meilleure distance à la liste
+        best_distances.append(
+            hive.best_bee.distance
+        )  # Ajout de la meilleure distance à la liste
         print(f"Generation {generation}: Best distance = {hive.best_bee.distance}")
 
     print(f"Best path found after {generation} generations:", hive.best_bee.path)
     print(f"Total mutations: {hive.total_mutations}")
     print(f"Total bees generated: {hive.total_bees_generated}")
 
-    plot_field(hive.best_bee.flowers, BEEHIVE_POS) 
-    plot_best_path(hive.best_bee, BEEHIVE_POS, generation, best_distance, hive.total_mutations, hive.total_bees_generated)
+    plot_field(hive.best_bee.flowers, BEEHIVE_POS)
+    plot_best_path(
+        hive.best_bee,
+        BEEHIVE_POS,
+        generation,
+        best_distance,
+        hive.total_mutations,
+        hive.total_bees_generated,
+    )
     plot_best_distances(best_distances)
 
-if __name__ == "__main__": 
-    num_generations = 150 # Specify the number of generations you want to run
+
+if __name__ == "__main__":
+    num_generations = 150  # Specify the number of generations you want to run
     main(num_generations)
